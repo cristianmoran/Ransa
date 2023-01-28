a4e1fe30650e3d25a871a5d162cf6151431bf41d
Luis Rojas <lerojas@farmacias.peruanas.pe>
2023-01-28 10:28:19 -0500

    ubicaciones

--- app/src/main/java/com/app/develop/ransapp/ui/history/FragmentOne.kt
+++ app/src/main/java/com/app/develop/ransapp/ui/history/FragmentOne.kt
@@ -11,13 +11,36 @@ class FragmentOne: BaseFragment() {
 
     lateinit var binding: FragmentOneBinding
 
+    private var statusSearch:Boolean = false
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentOneBinding.inflate(inflater, container, false)
-        return binding.root
+        return binding.root.apply {
+            initView()
+        }
+    }
+
+    private fun initView() {
+        binding.btnSearch.setOnClickListener {
+            if(!statusSearch){
+                statusSearch = true
+                visibleSearchConst()
+            }else{
+                statusSearch = false
+                goneSearchConst()
+            }
+        }
+    }
+
+    private fun visibleSearchConst(){
+        binding.constSearch.visibility= View.VISIBLE
+    }
+    private fun goneSearchConst(){
+        binding.constSearch.visibility= View.GONE
     }
 
 }
