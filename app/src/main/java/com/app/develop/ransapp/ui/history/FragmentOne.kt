2864e997e09e6ce31fff3bd9397e5d81e100879f
Luis Rojas <lerojas@farmacias.peruanas.pe>
2023-01-28 12:55:17 -0500

    update

--- app/src/main/java/com/app/develop/ransapp/ui/history/FragmentOne.kt
+++ app/src/main/java/com/app/develop/ransapp/ui/history/FragmentOne.kt
@@ -1,11 +1,15 @@
 package com.app.develop.ransapp.ui.history
 
+import android.app.Dialog
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.widget.AppCompatSpinner
+import com.app.develop.ransapp.R
 import com.app.develop.ransapp.base.BaseFragment
+import com.app.develop.ransapp.base.adapter.SpinnerAdapter
 import com.app.develop.ransapp.databinding.FragmentOneBinding
+import com.app.develop.ransapp.entity.SpinnerEntity
+
 
 class FragmentOne: BaseFragment() {
 
@@ -24,15 +28,68 @@ class FragmentOne: BaseFragment() {
         }
     }
 
+    private fun showDialog() {
+        val dialog = Dialog(requireActivity())
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        //dialog.setCancelable(false)
+        dialog.setContentView(R.layout.custom_dialog_search)
+        val spinnerEstados = dialog.findViewById(R.id.spinnerEstados) as AppCompatSpinner
+        val spinnerUbicaciones = dialog.findViewById(R.id.spinnerUbicaciones) as AppCompatSpinner
+        val spinnerTipos = dialog.findViewById(R.id.spinnerTipos) as AppCompatSpinner
+        val spinnerCampo = dialog.findViewById(R.id.spinnerCampo) as AppCompatSpinner
+        val spinnerAsc = dialog.findViewById(R.id.spinnerAsc) as AppCompatSpinner
+
+
+        val dataListEstados = mutableListOf<SpinnerEntity>()
+        dataListEstados.add(SpinnerEntity(0,"--Estado--"))
+        val dataListUbicaciones= mutableListOf<SpinnerEntity>()
+        dataListUbicaciones.add(SpinnerEntity(0,"--Ubicaciones--"))
+        val dataListTipos = mutableListOf<SpinnerEntity>()
+        dataListTipos.add(SpinnerEntity(0,"--Tipos--"))
+        val dataListCampos = mutableListOf<SpinnerEntity>()
+        dataListCampos.add(SpinnerEntity(0,"--Campo--"))
+        val dataListAsc = mutableListOf<SpinnerEntity>()
+        dataListAsc.add(SpinnerEntity(0,"--Asc--"))
+
+
+        val adapterEstados= SpinnerAdapter(context = requireContext(), dataListEstados)
+        spinnerEstados.adapter = adapterEstados
+
+        val adapterUbicaciones = SpinnerAdapter(context = requireContext(), dataListUbicaciones)
+        spinnerUbicaciones.adapter = adapterUbicaciones
+
+        val adapterTipos = SpinnerAdapter(context = requireContext(), dataListTipos)
+        spinnerTipos.adapter = adapterTipos
+
+        val adapterCampo = SpinnerAdapter(context = requireContext(), dataListCampos)
+        spinnerCampo.adapter = adapterCampo
+
+        val adapterAsc = SpinnerAdapter(context = requireContext(), dataListAsc)
+        spinnerAsc.adapter = adapterAsc
+
+
+        val window: Window? = dialog.window
+        window?.setLayout(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.WRAP_CONTENT
+        )
+        window?.setGravity(Gravity.CENTER)
+
+        dialog.show()
+
+    }
+
     private fun initView() {
         binding.btnSearch.setOnClickListener {
-            if(!statusSearch){
+
+            showDialog()
+            /*if(!statusSearch){
                 statusSearch = true
                 visibleSearchConst()
             }else{
                 statusSearch = false
                 goneSearchConst()
-            }
+            }*/
         }
     }
 
